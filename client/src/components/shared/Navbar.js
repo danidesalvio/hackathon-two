8e0091b3e650e7f84bd84995cb08fd1e34be1f03
Dani DeSalvio <danidvisuals@gmail.com>
2020-01-04 13:55:17 -0700

    danis branch styling and added location crud

--- client/src/components/shared/Navbar.js
+++ client/src/components/shared/Navbar.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import '../css/custom.css';
+import logo from '../images/airplane.svg';
 
 const Navbar = () => (
-  <>
-  </>
+  <div class="nav">
+      <div class="nav-logo">
+        <img src={logo}/>
+      </div>
+      <div class="nav-items">
+          <a href='/'>Home</a>
+      </div>
+  </div>
 )
 
 export default Navbar;
