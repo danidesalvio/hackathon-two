1acdb0405baa7b977c57638dd807c05818d7d25c
Dani DeSalvio <danidvisuals@gmail.com>
2020-01-04 16:02:24 -0700

    styling home

--- client/src/components/location/Location.js
+++ client/src/components/location/Location.js
@@ -4,34 +4,34 @@ import axios from 'axios';
 class Locations extends Component {
     state = { locations: [], trip: [], adding: false }
     
-    componentDidMount() {
-      const { id } = this.props.location.state
-      let tripId = id
-      axios.get(`/api/trips/${tripId}/locations`)
-        .then( res => {
-          this.setState({ locations: res.data })
-        })
-        .catch(err => {
-          console.logg(err)
-        })
-    }
+    // componentDidMount() {
+    //   const { id } = this.props.location.state
+    //   let tripId = id
+    //   axios.get(`/api/trips/${tripId}/locations`)
+    //     .then( res => {
+    //       this.setState({ locations: res.data })
+    //     })
+    //     .catch(err => {
+    //       console.logg(err)
+    //     })
+    // }
 
-    toggleAdd = () => this.setState({ adding: !this.state.adding })
+    // toggleAdd = () => this.setState({ adding: !this.state.adding })
 
-    addLocation = (location) => {
-      axios.post(`/api/trips/${tripId}/locations`, location)
-      .then( res => {
-        const { locations } = this.state
-        this.setState({ locations: [...locations, res.data] })
-      })
-      .catch( err => {
-        console.log(err)
-      })
-    }
+    // addLocation = (location) => {
+    //   axios.post(`/api/trips/${tripId}/locations`, location)
+    //   .then( res => {
+    //     const { locations } = this.state
+    //     this.setState({ locations: [...locations, res.data] })
+    //   })
+    //   .catch( err => {
+    //     console.log(err)
+    //   })
+    // }
 
-    updateTrip = (id, location) => {
-      axios.put(`/api/trips/${tripId}/locations/:id`, location)
-    }
+    // updateTrip = (id, location) => {
+    //   axios.put(`/api/trips/${tripId}/locations/:id`, location)
+    // }
 
 
     render() {
